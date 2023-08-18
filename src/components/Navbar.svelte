e33b3e85c232043300c9219267c878276a72bd4f
Leo <thcheetah777@gmail.com>
2023-08-18 21:57:48 +0300

    move to svelte (kinda)

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,31 @@
+<nav class="flex justify-between fixed w-full h-navbar z-50 p-xs items-center text-white">
+  <!-- Title -->
+  <header class="font-fancy text-3xl group">
+    <a class="group-hover:-top-1.5 top-0 relative duration-150" href="/">世界是学校</a>
+  </header>
+
+  <!-- Navigation -->
+  <div class="emphasis text-xs space-x-6">
+    <a href="/">Home</a>
+    <a href="/destinations">Destinations</a>
+    <a href="/blog">Blog</a>
+    <a href="/gallery">Gallery</a>
+    <a href="/about">About</a>
+  </div>
+
+  <!-- Socials -->
+  <div class="space-x-2">
+    <a href="https://www.facebook.com/worldschool.atlas.academy" class="group" target="_blank">
+      <!-- <Icon name="mdi:facebook" class="w-6 h-6 inline group-hover:-top-1.5 top-0 relative duration-150" /> -->
+      <span class="w-6 h-6 inline group-hover:-top-1.5 top-0 relative duration-150">⛄</span>
+    </a>
+    <a href="https://open.spotify.com/show/7xuJTB7kCfKB0JVBkgW4k3" class="group" target="_blank">
+      <!-- <Icon name="mdi:spotify" class="w-6 h-6 inline group-hover:-top-1.5 top-0 relative duration-150" /> -->
+      <span class="w-6 h-6 inline group-hover:-top-1.5 top-0 relative duration-150">⛄</span>
+    </a>
+    <a href="https://podcasts.apple.com/us/podcast/%E4%B8%96%E7%95%8C%E6%98%AF%E5%AD%B8%E6%A0%A1-worldschooling/id1646258789" class="w-6 h-6 group" target="_blank">
+      <!-- <Icon name="fa:podcast" class="w-6 h-6 inline group-hover:-top-1.5 top-0 relative duration-150" /> -->
+      <span class="w-6 h-6 inline group-hover:-top-1.5 top-0 relative duration-150">⛄</span>
+    </a>
+  </div>
+</nav>
