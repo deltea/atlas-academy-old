c8f1f9dfa2958d6e79d520eade12176d070d6494
Leo <thcheetah777@gmail.com>
2023-08-19 09:06:55 +0300

    better destinations and posts

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -25,10 +25,12 @@ export interface Destination {
   }
 }
 
-export const contentfulClient = contentful.createClient({
+const contentfulBaseClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.DEV
     ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
+
+export const contentfulClient = contentfulBaseClient.withoutUnresolvableLinks;
