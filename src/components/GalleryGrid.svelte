a979ba3e7c7e7fd15f090edd6e684c953c247506
Leo <thcheetah777@gmail.com>
2023-08-30 12:57:37 +0300

    fix gallery

--- src/components/GalleryGrid.svelte
+++ src/components/GalleryGrid.svelte
@@ -13,12 +13,16 @@
   let currentPhoto: Entry<GalleryPhoto, "WITHOUT_UNRESOLVABLE_LINKS", string>;
   let loading = false;
   let currentDestination = "all";
+  let currentGallery: Entry<GalleryPhoto, "WITHOUT_UNRESOLVABLE_LINKS", string>[];
 
-  $: currentPhoto = entries.items[currentIndex];
+  $: currentPhoto = currentGallery[currentIndex];
   $: {
     currentIndex;
     loading = true;
   }
+  $: currentGallery = currentDestination === "all" ? entries.items : entries.items.filter(item =>
+    item.fields.country?.fields.short === currentDestination
+  )
 
   function lightDismiss(e: MouseEvent) {
     const target = e.target as HTMLDialogElement;
@@ -37,7 +41,7 @@
   }
 
   function changePhoto(direction: -1 | 1) {
-    currentIndex = clamp(currentIndex + direction, 0, entries.items.length - 1);
+    currentIndex = clamp(currentIndex + direction, 0, currentGallery.length - 1);
   }
 
   function keyPress(e: KeyboardEvent) {
@@ -72,17 +76,15 @@
   {/if}
 </section>
 
-{#if entries.items.filter(item =>
-  item.fields.country?.fields.short === currentDestination
-).length === 0 && currentDestination !== "all"}
+{#if currentGallery.length === 0}
   <div class="flex justify-center items-center h-40">
     <p>Oops! Looks like no photos were found in {currentDestination}.</p>
   </div>
 {/if}
 
-<section class="grid grid-cols-5 dark:gap-2 gap-1 duration-200">
-  {#each entries.items as item, i (item.fields.slug)}
-    {#if item.fields.country?.fields.short === currentDestination || currentDestination === "all"}
+{#if currentGallery}
+  <section class="grid grid-cols-5 dark:gap-2 gap-1 duration-200">
+    {#each currentGallery as item, i (item.fields.slug)}
       <button on:click={() => openModal(i)} class="group">
         <div style:background-image={`url('${image(item.fields.image?.fields.file?.url, 400)}')`}
           class="bg-cover bg-center aspect-square group-hover:bg-neutral-600 bg-blend-multiply duration-200 relative">
@@ -92,63 +94,65 @@
           </div>
         </div>
       </button>
-    {/if}
-  {/each}
-</section>
+    {/each}
+  </section>
+{/if}
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-<dialog bind:this={modal}
-  on:click={lightDismiss}
-  on:keydown={keyPress}
-  class="fixed backdrop:bg-black backdrop:bg-opacity-80 w-full h-full bg-transparent text-white flex justify-center items-center outline-none">
-  <button on:click={closeModal} class="absolute right-2 top-2">
-    <iconify-icon icon="mdi:close" class="text-3xl"></iconify-icon>
-  </button>
-
-  <div class="pointer-events-none flex justify-center h-4/5 items-center gap-8">
-    {#if currentIndex > 0}
-      <button class="h-full px-8 group pointer-events-auto"
-        on:click={() => changePhoto(-1)}
-        transition:scale={{ duration: 200 }}>
-        <iconify-icon icon="mdi:chevron-left" class="text-4xl group-hover:scale-125 group-hover:-translate-x-2 group-active:scale-90 duration-150 origin-center"></iconify-icon>
-      </button>
-    {/if}
-
-    {#if loading}
-      <div class="bg-white dark:bg-neutral min-w-[30rem] h-[30rem] flex justify-center items-center pointer-events-auto">
-        <iconify-icon icon="ph:spinner-bold"
-          class="text-4xl animate-spin text-neutral dark:text-white"></iconify-icon>
+{#if currentPhoto}
+  <dialog bind:this={modal}
+    on:click={lightDismiss}
+    on:keydown={keyPress}
+    class="fixed backdrop:bg-black backdrop:bg-opacity-80 w-full h-full bg-transparent text-white flex justify-center items-center outline-none">
+    <button on:click={closeModal} class="absolute right-2 top-2">
+      <iconify-icon icon="mdi:close" class="text-3xl"></iconify-icon>
+    </button>
+  
+    <div class="pointer-events-none flex justify-center h-4/5 items-center gap-8">
+      {#if currentIndex > 0}
+        <button class="h-full px-8 group pointer-events-auto"
+          on:click={() => changePhoto(-1)}
+          transition:scale={{ duration: 200 }}>
+          <iconify-icon icon="mdi:chevron-left" class="text-4xl group-hover:scale-125 group-hover:-translate-x-2 group-active:scale-90 duration-150 origin-center"></iconify-icon>
+        </button>
+      {/if}
+  
+      {#if loading}
+        <div class="bg-white dark:bg-neutral min-w-[30rem] h-[30rem] flex justify-center items-center pointer-events-auto">
+          <iconify-icon icon="ph:spinner-bold"
+            class="text-4xl animate-spin text-neutral dark:text-white"></iconify-icon>
+        </div>
+      {/if}
+  
+      <img src={image(currentPhoto.fields.image?.fields.file?.url, 1000)}
+        alt={currentPhoto.fields.title}
+        class="w-2/5 pointer-events-auto"
+        class:hidden={loading}
+        on:load={() => loading = false}>
+  
+      <div class="w-3/5 space-y-8 mt-6 h-full pointer-events-auto">
+        <h1 class="font-bold text-2xl">{currentPhoto.fields.title}</h1>
+        <small class="italic text-base">
+          {currentPhoto.fields.city},
+          <a href="/destinations/{currentPhoto.fields.country?.fields.slug}"
+            class="hover:text-theme duration-200">
+            {currentPhoto.fields.country?.fields.short}
+          </a>
+        </small>
+        <h2 class="text-lg">{currentPhoto.fields.description}</h2>
       </div>
-    {/if}
-
-    <img src={image(currentPhoto.fields.image?.fields.file?.url, 1000)}
-      alt={currentPhoto.fields.title}
-      class="w-2/5 pointer-events-auto"
-      class:hidden={loading}
-      on:load={() => loading = false}>
-
-    <div class="w-3/5 space-y-8 mt-6 h-full pointer-events-auto">
-      <h1 class="font-bold text-2xl">{currentPhoto.fields.title}</h1>
-      <small class="italic text-base">
-        {currentPhoto.fields.city},
-        <a href="/destinations/{currentPhoto.fields.country?.fields.slug}"
-          class="hover:text-theme duration-200">
-          {currentPhoto.fields.country?.fields.short}
-        </a>
-      </small>
-      <h2 class="text-lg">{currentPhoto.fields.description}</h2>
+  
+      {#if currentIndex < currentGallery.length - 1}
+        <button class="h-full px-8 group pointer-events-auto"
+          on:click={() => changePhoto(1)}
+          transition:scale={{ duration: 200 }}>
+          <iconify-icon icon="mdi:chevron-right" class="text-4xl group-hover:scale-125 group-hover:translate-x-2 group-active:scale-90 duration-150 origin-center"></iconify-icon>
+        </button>
+      {/if}
     </div>
-
-    {#if currentIndex < entries.items.length - 1}
-      <button class="h-full px-8 group pointer-events-auto"
-        on:click={() => changePhoto(1)}
-        transition:scale={{ duration: 200 }}>
-        <iconify-icon icon="mdi:chevron-right" class="text-4xl group-hover:scale-125 group-hover:translate-x-2 group-active:scale-90 duration-150 origin-center"></iconify-icon>
-      </button>
-    {/if}
-  </div>
-</dialog>
+  </dialog>
+{/if}
 
 <style>
   dialog[open] {
